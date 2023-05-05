42748d80e1caf3937626ca8436e17d3e41e17064
lucifer1 <1198647915@qq.com>
2023-05-05 23:47:12 +0800

    gitignore

--- dist/sales/sales.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Model } from 'mongoose';
-import { Sale } from './schemas/sale.schema';
-export declare class SalesService {
-    private saleModel;
-    constructor(saleModel: Model<Sale>);
-    findAll(): Promise<Sale[]>;
-    create(sale: Sale): Promise<Sale>;
-}
